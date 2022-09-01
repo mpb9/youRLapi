849521ff3b8e71f71433b9d5560e6ef81e04b495
Michael Beebe <mpb9@live.unc.edu>
2022-09-01 14:28:46 -0400

    profile update

--- user-apis/newprofileinfo.php
+++ user-apis/newprofileinfo.php
@@ -17,32 +17,82 @@ $email = $_POST['email'];
 $bio = $_POST['bio'];
 $img = $_POST['img'];
 
-try {
-  $sql = 'UPDATE user SET 
-      fullname = :fullname,
-      email = :email,
-      bio = :bio,
-      img = :img
-      WHERE name = :name';
-  $s = $pdo->prepare($sql);
-  $s->bindValue(':name', $name);
-  $s->bindValue(':fullname', $fullname);
-  $s->bindValue(':email', $email);
-  $s->bindValue(':bio', $bio);
-  $s->bindValue(':img', $img);
-  $s->execute();
 
-} catch (PDOException $e) {
-  $error = 'Error updating user info.';
-  echo $error;
+if(!validImage($img)){
+  try {
+    $sql = 'UPDATE user SET 
+        fullname = :fullname,
+        email = :email,
+        bio = :bio
+        WHERE name = :name';
+    $s = $pdo->prepare($sql);
+    $s->bindValue(':name', $name);
+    $s->bindValue(':fullname', $fullname);
+    $s->bindValue(':email', $email);
+    $s->bindValue(':bio', $bio);
+    $s->execute();
+
+  } catch (PDOException $e) {
+    $error = 'Error adding user info.';
+    echo $error;
+  }
+
+  try {
+    $sql = 'SELECT * FROM user 
+        WHERE name = :name';
+    $s = $pdo->prepare($sql);
+    $s->bindValue(':name', $name);
+    $s->execute();
+
+  } catch (PDOException $e) {
+    $error = 'Error getting new user info.';
+    echo $error;
+  }
+
+  while(($row = $s->fetch(PDO::FETCH_ASSOC)) != false){
+    $info[] = array(
+      'email' => $row['email'],
+      'fullname' => $row['fullname'],
+      'bio' => $row['bio'],
+      'img' => $row['img']
+    );
+  }
+
+} else {
+
+  try {
+    $sql = 'UPDATE user SET 
+        fullname = :fullname,
+        email = :email,
+        bio = :bio,
+        img = :img
+        WHERE name = :name';
+    $s = $pdo->prepare($sql);
+    $s->bindValue(':name', $name);
+    $s->bindValue(':fullname', $fullname);
+    $s->bindValue(':email', $email);
+    $s->bindValue(':bio', $bio);
+    $s->bindValue(':img', $img);
+    $s->execute();
+
+  } catch (PDOException $e) {
+    $error = 'Error adding user info (+ profile picture).';
+    echo $error;
+  }
+
+  $info[] = array(
+    'email' => $email,
+    'fullname' => $fullname,
+    'bio' => $bio,
+    'img' => $img
+  );
+
 }
-
-$info[] = array(
-  'email' => $email,
-  'fullname' => $fullname,
-  'bio' => $bio,
-  'img' => $img
-);
 
 echo json_encode($info[0]);
 // REALLY DONT HAVE TO RETURN... UPDATE IN FUTURE WHEN DONE TESTING
+
+function validImage($file) {
+  $size = getimagesize($file);
+  return (strtolower(substr($size['mime'], 0, 5)) == 'image' ? true : false);  
+}
